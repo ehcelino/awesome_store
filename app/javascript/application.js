d416791819b0dac3266127d72c3512b76bb764c8
Eduardo Celino <ehcelino@gmail.com>
2023-01-30 19:51:56 -0300

    actiontext

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,8 +2,12 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import "trix"
+import "@rails/actiontext"
+
 
 let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))  
 let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {  
   return new bootstrap.Popover(popoverTriggerEl)  
 })  
+
