1df3cd2f968be7e17c740fd7474fa6b5aec4927c
Eduardo Celino <ehcelino@gmail.com>
2023-02-02 14:57:07 -0300

    barra de navegação

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  require 'ostruct'
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
+    if @current_user
+        @current_user
+    else
+      OpenStruct.new(username: 'Visitante')
+    end
+  end
+  helper_method :current_user
+
 end
